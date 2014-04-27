191cc78f1ca4988158177ea9bf19e95cb8f48d72
HappyBerrysBoy <elytselyts@gmail.com>
2014-04-27 22:23:48 +0900

    Commit Test

--- linfo/src/linfo/project/terminalscraping/objects/TerminalWebSite.java
+++ linfo/src/linfo/project/terminalscraping/objects/TerminalWebSite.java
@@ -3,6 +3,7 @@ package linfo.project.terminalscraping.objects;
 import java.util.HashMap;
 
 // Terminal Web Site
+// KSC Comment
 public class TerminalWebSite {
 	private String id;
 	private String terminalName;
