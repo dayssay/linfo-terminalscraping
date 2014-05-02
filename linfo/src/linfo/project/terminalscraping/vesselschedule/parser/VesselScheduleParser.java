81d4e047bd5b9fbe12202e35f7e1e069be9b6d56
KSC <elytselyts@nate.com>
2014-05-02 20:54:53 +0900

    Parsing 함수에 주석(내용 설명)

--- linfo/src/linfo/project/terminalscraping/vesselschedule/parser/VesselScheduleParser.java
+++ linfo/src/linfo/project/terminalscraping/vesselschedule/parser/VesselScheduleParser.java
@@ -28,66 +28,82 @@ public class VesselScheduleParser{
 		return result;
 	}
 
+	// Berth Number
 	protected String getBerthNo(String html) {
 		return getData(html);
 	}
 
+	// Operator
 	protected String getOPR(String html) {
 		return getData(html);
 	}
 
+	// 항차
 	protected String getVVD(String html) {
 		return getData(html);
 	}
 
+	// 선사 In 항차
 	protected String getINVVDforShippingCom(String html) {
 		return getData(html);
 	}
 
+	// 선사 Out 항차
 	protected String getOUTVVDforShippingCom(String html) {
 		return getData(html);
 	}
 
+	// 선박명
 	protected String getVSLName(String html) {
 		return getData(html);
 	}
 
+	// Route
 	protected String getRoute(String html) {
 		return getData(html);
 	}
 
+	// Cargo Closing Time(반입 마감시간)
 	protected String getCCT(String html) {
 		return getData(html);
 	}
 
+	// Expected Time of Berthing(입항 예정시간)
 	protected String getETB(String html) {
 		return getData(html);
 	}
 
+	// Expected Time of Departure(출할 예정 시간)
 	protected String getETD(String html) {
 		return getData(html);
 	}
 
+	// Actual Time of Berthing(실제 입항 시간)
 	protected String getATB(String html) {
 		return getData(html);
 	}
 
+	// Actual Time of Departure(실제 출항 시간)
 	protected String getATD(String html) {
 		return getData(html);
 	}
 
+	// 선적 물량
 	protected String getLOADCnt(String html) {
 		return getData(html);
 	}
 
+	// 양하 물량
 	protected String getDISCnt(String html) {
 		return getData(html);
 	}
 
+	// Shifting 물량
 	protected String getShiftCnt(String html) {
 		return getData(html);
 	}
 
+	// 접안 상태(접안전, 접안중, 출항)
 	protected String getVVDStatus(String html) {
 		return getData(html);
 	}
