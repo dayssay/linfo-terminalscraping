4b5ff68084570191b8d2fc46ad1bc998ab19f751
purepleya <purepleya@jh_notebook>
2014-05-15 07:12:08 +0900

    Oracle Driver Test

--- linfo/src/linfo/project/terminalscraping/Driver.java
+++ linfo/src/linfo/project/terminalscraping/Driver.java
@@ -1,13 +1,25 @@
 package linfo.project.terminalscraping;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Calendar;
+import java.util.Formatter;
+
+import oracle.jdbc.pool.OracleDataSource;
 import linfo.project.terminalscraping.objects.TerminalWebSite;
 import linfo.project.terminalscraping.scrapper.Scraper;
 import linfo.project.terminalscraping.vesselschedule.parser.VesselScheduleParser;
 import linfo.project.terminalscraping.vesselschedule.parser.VesselScheduleParserFactory;
 
 public class Driver {
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception{
 		Scraper s = new Scraper();
+		OracleDataSource ods = new OracleDataSource();
+		ods.setURL("jdbc:oracle:thin:@hnctech73.iptime.org:1521:ora11g");
+		ods.setUser("biguser");
+		ods.setPassword("biguser");
+		Connection conn = ods.getConnection();
+		Calendar c = Calendar.getInstance();
 		
 		for(String item : s.getItems()){
 			for(TerminalWebSite t : s.getTerminalList(item)){
@@ -15,11 +27,27 @@ public class Driver {
 				VesselScheduleParser parser = new VesselScheduleParserFactory().getParser(t.getId());
 				
 				if(parser != null){
+					String sql = "INSERT INTO T_RAW_HTML(SCRAP_DT, SCRAP_TM, TERMINAL_ID, HTML)"
+								+ "VALUES(?, ?, ?, ?)";
+					PreparedStatement pstmt = conn.prepareStatement(sql);
+					String dt = String.format("%04d", c.YEAR) 
+							+ String.format("%02d", c.MONTH+1)
+							+ String.format("%02d", c.DATE);
+					String tm = String.format("%02", c.HOUR_OF_DAY)
+							+ String.format("%02", c.MINUTE);
+					
+					
+//					String a = String.format("%02d", 12);
+					
+					
+					
+					
+					
 //					Scraper가 html 코드를 가져오고
 //					Parser 각 html 코드로 부터 데이터를 뽑아옴
-					System.out.println("=====================" + t.getId() + "=====================================");
+//					System.out.println("=====================" + t.getId() + "=====================================");
 //					System.out.println(s.getHtml(t));
-					parser.SetBerthInfo(s.getHtml(t));
+//					parser.SetBerthInfo(s.getHtml(t));
 				}
 			}
 		}
